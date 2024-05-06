423c752ba49da56183215ee5a4277934be789284
opilane <opilane@HP-CZC0359NNL>
2024-05-06 16:29:13 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Inside the for loop: " + i);
+                AnotherMethod(i); 
+            }
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+        static void AnotherMethod(int num)
+        {
+            Console.WriteLine("Another method called with: " + num);
+        }
+    }
+}
